4f1323950d5db91d59b8aec8bf6dec0d9093f039
ma <ma_yamaki@yahoo.com>
2019-08-17 09:37:58 +0900

    mmd

--- A_Z/data_Prepro/r/missing_data.R
+++ A_Z/data_Prepro/r/missing_data.R
@@ -18,4 +18,18 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased <- factor(dataset$Purchased,
                             levels = c("No", "Yes"),
                             labels = c(0,1))
-dataset
+
+install.packages("caTools")
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+training_set = subset(dataset, split == T)
+test_set = subset(dataset, split == F)
+
+# feature scaling
+
+training_set[,2:3] <- scale(training_set[,2:3])
+test_set[,2:3] <- scale(test_set[,2:3])
+training_set
