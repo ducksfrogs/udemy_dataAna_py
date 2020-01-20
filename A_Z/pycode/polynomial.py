aac50bcf49a0f7c1f9616bdff8b7ae4184da6adc
ma <ma_yamaki@yahoo.com>
2020-01-20 11:29:21 +0900

    mmm

---
+++ A_Z/pycode/polynomial.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('../data/Position_Salaries.csv')
+
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+"""
+
+#Fitting Liniar Region
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+#polynomial Regression
+
+from sklearn.preprocessing import PolynomialFeatures
+
+poly_leg = PolynomialFeatures(degree=2)
+X_poly = poly_leg.fit_transform(X)
+lin_reg2 = LinearRegression()
+
+lin_reg2.fit(X_poly, y)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title("Truth or bruff")
+plt.xlabel('Postition')
+plt.ylabel("salary")
+plt.show()
